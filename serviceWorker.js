e548ceeccec0074bb6f516ec064ceddd0bcb2822
AgniPro <84276343+AgniPro@users.noreply.github.com>
2021-07-13 18:24:40 +0530

    Add files via upload

---
+++ serviceWorker.js
@@ -0,0 +1,52 @@
+var cacheAll = false;
+var CACHE_NAME = 'webapk-cache';
+var urlsToCache = [
+	'https://learngraduation.blogspot.com/',
+	'https://1.bp.blogspot.com/-tdKcjuPBzzk/YKO4BVVNIFI/AAAAAAAACXM/ZcW2I4Pr_701MB4J-4HDj8p9uo56Srn8gCLcBGAsYHQ/s250/learngraduationlogo.webp',
+	'https://1.bp.blogspot.com/-tdKcjuPBzzk/YKO4BVVNIFI/AAAAAAAACXM/ZcW2I4Pr_701MB4J-4HDj8p9uo56Srn8gCLcBGAsYHQ/s250/learngraduationlogo.webp',
+];
+var urlsNotToCache = [
+];
+
+// Install Event
+self.addEventListener('install', function(event) {
+	console.log("[SW] install event: ",event);
+	// Perform install steps
+	event.waitUntil(
+		caches.open(CACHE_NAME).then(
+			function(cache) {
+				console.log('[SW] Opened cache: ',cache);
+				return cache.addAll(urlsToCache);
+			}
+		)
+	);
+});
+
+
+// Fetch Event
+self.addEventListener('fetch', function(event) {
+	console.log("[SW] fetch event: ",event);
+	event.respondWith(
+		caches.match(event.request).then(
+			function(response) {
+				if (response) return response;
+				else if (!cacheAll || urlsNotToCache.indexOf(event.request) !== -1) return fetch(event.request);
+				else {
+					fetch(event.request).then(
+						function(response) {
+							if(!response || response.status !== 200 || response.type !== 'basic') return response;
+							var responseToCache = response.clone();
+							caches.open(CACHE_NAME).then(
+								function(cache) {
+									cache.put(event.request, responseToCache);
+								}
+							);
+							return response;
+						}
+					);
+				}
+			}
+		)
+	);
+});
+
